5accd9a919085d37b174f8d4612c648a275a9e08
Jarigo <johnarigo96706@gmail.com>
2022-11-15 14:56:34 -0800

    added keys to n ews articles/ updated prisma

--- components/news.tsx
+++ components/news.tsx
@@ -11,11 +11,8 @@ export default function News({ news }: newsType) {
       {news.articles.map((article) => {
         console.log(article);
         return (
-          <Link href={article.url}>
-            <div
-              key={article.url}
-              className="flex flex-col items-center justify-center w-full h-auto bg-base-content text-info-content pb-10 mt-5 rounded-lg"
-            >
+          <Link href={article.url} key={article.url}>
+            <div className="flex flex-col items-center justify-center w-full h-auto bg-base-content text-info-content pb-10 mt-5 rounded-lg">
               <h2 className="mt-3 text-center mb-3 text-md">{article.title}</h2>
               <img src={article.urlToImage} />
             </div>
