dd07ed367ffb655b2e5ad05cd3713368608dc6dd
Jarigo <johnarigo96706@gmail.com>
2022-12-08 16:36:33 -0800

    added keys to .map iterations

--- components/practice/practice2/favoriteQuotes.tsx
+++ components/practice/practice2/favoriteQuotes.tsx
@@ -8,7 +8,10 @@ export const FavoriteQuotes = ({ quotes }: favoriteType) => {
       <div className="h-full overflow-y-auto">
         {quotes.map((quote, index) => {
           return (
-            <div className="w-96 h-56 bg-white text-black flex flex-col items-center justify-center mt-5">
+            <div
+              key={index}
+              className="w-96 h-56 bg-white text-black flex flex-col items-center justify-center mt-5"
+            >
               <h1 className="text-center">{quote.text}</h1>
               <h3 className="self-end mr-10 italic text-gray-700">
                 - {quote.author}
