461cc268f0fec80dd3f5d7c4cdacdf6859e7076b
Jarigo <johnarigo96706@gmail.com>
2022-12-10 22:20:10 -0800

    changed translation for only desktop

--- components/transition.tsx
+++ components/transition.tsx
@@ -8,7 +8,7 @@ export const TransitionY = ({ children, execute }: TransitionType) => {
   return (
     <div
       className={`w-full h-full flex justify-center items-center transition-all duration-1000 ease-in transform ${
-        execute ? `opacity-100 translate-y-14` : "opacity-0 translate-y-0"
+        execute ? `opacity-100 sm:translate-y-14` : "opacity-0 sm:translate-y-0"
       }`}
     >
       {children}
