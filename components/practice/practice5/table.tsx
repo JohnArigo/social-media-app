765bd59509dabe5cde6a970ae7c5615dcfe80bb2
Jarigo <johnarigo96706@gmail.com>
2022-12-10 22:08:30 -0800

    created table component

---
+++ components/practice/practice5/table.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+export type TableType = {
+  url?: string;
+};
+
+export const Table = ({ url }: TableType) => {
+  const [data, setData] = useState<any>([
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+      age: 35,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      email: "jane.doe@example.com",
+      age: 32,
+    },
+  ]);
+
+  useEffect(() => {
+    if (url !== undefined) {
+      alert("loading data");
+      try {
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => setData(data.data));
+      } catch (error) {
+        alert(error);
+      }
+    }
+  }, [url]);
+
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <table className="bg-base-content w-96 sm:w-2/3 text-info-content">
+        <tr className="w-full text-start">
+          {Object.keys(data[0]).map((name: string) => {
+            return <th className={`1/${data.length}`}>{name}</th>;
+          })}
+        </tr>
+        {data.map((item: any) => {
+          return (
+            <tr className="w-full text-center">
+              {Object.values(item).map((value: any) => {
+                return <td>{value}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </table>
+    </main>
+  );
+};
+export default Table;
--- pages/displayPracticeComponentPage.tsx
+++ pages/displayPracticeComponentPage.tsx
@@ -3,6 +3,7 @@ import { Meme } from "../components/practice/practice1/meme";
 import { RandomQuote } from "../components/practice/practice2/quote";
 import NewsPractice from "../components/practice/practice3/news";
 import UserForm from "../components/practice/practice4/userForm";
+import Table from "../components/practice/practice5/table";
 import { ArticleRoot } from "../lib/types";
 
 // export async function getStaticProps() {
@@ -22,10 +23,11 @@ import { ArticleRoot } from "../lib/types";
 
 export const PracticePage = () => {
   // const [newsData, setNewsData] = useState<ArticleRoot>(news);
-
+  const url =
+    "https://datausa.io/api/data?drilldowns=Nation&measures=Population";
   return (
     <main>
-      <UserForm />
+      <Table url={url} />
     </main>
   );
 };
