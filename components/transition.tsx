c1f2ba734a0cf38fb29502be3017fc06aa35fc75
Jarigo <johnarigo96706@gmail.com>
2022-12-10 23:16:49 -0800

    changed transition zindex to 50

--- components/transition.tsx
+++ components/transition.tsx
@@ -7,7 +7,7 @@ export type TransitionType = {
 export const TransitionY = ({ children, execute }: TransitionType) => {
   return (
     <div
-      className={`w-full h-full flex justify-center items-center transition-all duration-1000 ease-in transform ${
+      className={`z-50 w-full h-full flex justify-center items-center transition-all duration-1000 ease-in transform ${
         execute ? `opacity-100 sm:translate-y-14` : "opacity-0 sm:translate-y-0"
       }`}
     >
